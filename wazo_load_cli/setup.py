78be8ab72b7680c40514f12ed8610d4966ae7dc2
franckrupin <frupin@protonmail.com>
2023-06-01 10:48:39 -0400

    Configure tests and setup the cli

---
+++ wazo_load_cli/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from setuptools import setup
+
+setup(
+    name='wlctl',
+    version='1.0.0',
+    description='Wazo Load CLI',
+    author='Wazo Authors',
+    author_email='dev@wazo.community',
+    url='http://wazo.community',
+    license='GPLv3',
+    py_modules=['wlctl'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points={
+        'console_scripts': [
+            'wlctl = wlctl:cli',
+        ],
+    },
+)
