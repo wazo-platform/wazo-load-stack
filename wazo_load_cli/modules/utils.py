63d80682975b014b76f843789ca3e6d0dfea8c8e
franckrupin <frupin@protonmail.com>
2023-06-01 11:40:00 -0400

    Add modules and tests

---
+++ wazo_load_cli/modules/utils.py
@@ -0,0 +1,10 @@
+import configparser
+import os
+
+from typing import Any
+
+def load_config(config_file: str) -> Any:
+   config_file_path = os.path.expanduser(config_file)
+   config = configparser.ConfigParser()
+   config.read(config_file_path)
+   return config
---
+++ wazo_load_cli/tests/test_load_generator.py
@@ -0,0 +1,71 @@
+import os
+import configparser
+from ..modules.load_generator import LoadGenerator, Timer
+
+class MockTimer(Timer):
+    def get_timer(self, delay: int = 60) -> int:
+        return 28
+
+
+def test_generate_load_files(tmpdir, capfd):
+    out, err = capfd.readouterr()
+
+    config = configparser.ConfigParser()
+    config["DEFAULT"] = {
+        "DEBUG": "1",
+        "DISABLE_CHATD": "0",
+        "DURATION": "300",
+        "TOKEN_EXPIRATION": "600",
+        "DELAY_CNX_RAND": "60",
+        "TTL": "30",
+        "SERVER": "router-1.load.wazo.io",
+        "LOAD_SECTIONS": "1",
+        "CLIENTS": "1",
+        "CONTAINERS": "100",
+        "REQUEST_TIMEOUT": "300000",
+        "DISABLE_HEADER_CHECK": "1",
+        "TRAFGEN_NUMBER": "1",
+        "LOAD_FILES_NUMBER": "1",
+        "EXT": "@wazo.io"
+    }
+
+    config_file = tmpdir.join("genwda-load.conf")
+    with open(config_file, "w") as f:
+        config.write(f)
+
+    os.chdir(tmpdir)
+
+    mock_timer = MockTimer()
+    load = LoadGenerator(config_file, "wda", mock_timer)
+    load.generate_load_files()
+    load_file = "wda1.yml"
+
+    expected_content = """loads:
+  - load:
+    - node:
+      host: trafgen1.load.wazo.io
+      container: wda-load-test1
+      cmd: "sleep 28 && node /usr/src/app/index.js"
+      env:
+        LOGIN: 1001@wazo.io
+        PASSWORD: superpass
+        SERVER: router-1.load.wazo.io
+        SESSION_DURATION: 300
+        DEBUG: 1
+        TOKEN_EXPIRATION: 600
+        DISABLE_CHATD: 0
+        DISABLE_HEADER_CHECK: 1
+        REQUEST_TIMEOUT: 300000
+        DOCKER: 1
+    ttl: 30
+    tag: wda-load
+    compose: /etc/trafgen/Docker-compose.yml
+    forever: True
+"""
+
+    with open(load_file) as f:
+        content = f.read()
+        print(content)
+        print(out)
+        print(err)
+        assert content == expected_content
