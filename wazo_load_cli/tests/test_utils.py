61c8c794566c4f1bff62b71c45e919b3b4a61894
franckrupin <frupin@protonmail.com>
2023-06-06 10:18:17 -0400

    Fix tox environment

--- wazo_load_cli/tests/test_utils.py
+++ wazo_load_cli/tests/test_utils.py
@@ -26,15 +26,14 @@ def test_send_json(requests_mock):
     requests_mock is a fixture provided by the requests-mock plugin."""
     data = {"key": "value"}
     url = "https://wazo.load.stack/api"
-    
+
     # Setup the mock requests to intercept all POST at the url destination and return s a 200.
     requests_mock.post(url, status_code=200)
-    
+
     send_json(data, url)
-    
+
     assert requests_mock.called
     assert requests_mock.last_request.url == url
-    
+
     assert isinstance(requests_mock.last_request.json(), str)
     assert requests_mock.last_request.json() == json.dumps(data)
-    
