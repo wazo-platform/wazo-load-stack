ea9d75469260be83274a98588405a99243cfd551
franckrupin <frupin@protonmail.com>
2023-06-21 11:05:58 -0400

    Add test files

---
+++ wazo_load_pilot/tests/simple_serve.py
@@ -0,0 +1,35 @@
+import http.server
+import json
+import ssl
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    def do_POST(self):
+        content_length = int(self.headers.get('Content-Length', 0))
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        
+
+        print("JSON reçu :")
+        print(post_data)
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'POST request received')
+
+address = '127.0.0.1'
+port = 443
+
+certfile = 'tests/certificate.pem'
+keyfile = 'tests/private.key'
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain(certfile, keyfile)
+
+httpd = http.server.HTTPServer((address, port), CustomHandler)
+httpd.socket = context.wrap_socket(httpd.socket)
+
+print(f'Serveur HTTPS en cours d\'exécution sur {address}:{port}')
+
+httpd.serve_forever()
+
+
