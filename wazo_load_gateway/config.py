43fa88a55628b842b24026cbc169fb45a4f52b26
franckrupin <frupin@protonmail.com>
2023-08-23 07:51:30 -0400

    Add srvice definition

---
+++ wazo_load_gateway/config.py
@@ -0,0 +1,69 @@
+# Copyright 2023 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import argparse
+
+from xivo.chain_map import ChainMap
+from xivo.config_helper import read_config_file_hierarchy
+
+_DEFAULT_HTTPS_PORT = 9900
+
+_DEFAULT_CONFIG = {
+    'config_file': '/etc/wazo-load-api/config.yml',
+    'debug': False,
+    'extra_config_files': '/etc/wazo-load-api/conf.d',
+    'log_file': '/var/log/wazo-load-api.log',
+    'log_level': 'info',
+    'user': 'wazo-api',
+    'rest_api': {
+        'listen': '0.0.0.0',
+        'port': _DEFAULT_HTTPS_PORT,
+        'certificate': '/etc/wazo-load-api/certificate.pem',
+        'private_key': '/etc/wazo-load-api/private.key',
+        'cors': {
+            'enabled': True,
+            'allow_headers': ['Content-Type', 'X-Auth-Token'],
+        },
+        'max_threads': 10,
+    },
+    'enabled_plugins': {
+        'job': True,
+        'status': True,
+    },
+    'certs': {
+        'csr': '/etc/wazo-load-api/certificate.csr',
+        'cert': '/etc/wazo-load-api/certificate.pem',
+        'key': '/etc/wazo-load-api/private.key',
+    },
+}
+
+
+def load_config(args):
+    cli_config = _parse_cli_args(args)
+    file_config = read_config_file_hierarchy(ChainMap(cli_config, _DEFAULT_CONFIG))
+    return ChainMap(file_config, _DEFAULT_CONFIG)
+
+
+def _parse_cli_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-c', '--config-file', action='store', help='The path to the config file'
+    )
+    parser.add_argument(
+        '-d',
+        '--debug',
+        action='store_true',
+        help='Log debug mesages. Override log_level',
+    )
+    parser.add_argument('-u', '--user', action='store', help='The owner of the process')
+    parsed_args = parser.parse_args(argv)
+
+    result = {}
+    if parsed_args.config_file:
+        result['config_file'] = parsed_args.config_file
+    if parsed_args.debug:
+        result['debug'] = parsed_args.debug
+    if parsed_args.user:
+        result['user'] = parsed_args.user
+
+    return result
---
+++ wazo_load_gateway/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Copyright 2023 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+NAME = 'wlapi'
+setup(
+    name=NAME,
+    version='1.0',
+    author='Wazo Authors',
+    description='Wazo Load API',
+    author_email='dev@wazo.io',
+    url='https://wazo-platform.org',
+    packages=find_packages(),
+    package_data={'plugins': ['*/api.yml']},
+    entry_points={
+        'console_scripts': [
+            f'{NAME}=main:main',
+        ],
+        'plugins': [
+            'job = plugins.job.plugin:Plugin',
+            'status = plugins.status.plugin:Plugin',
+        ],
+    },
+)
