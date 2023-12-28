1a92740122d1915d9df85b72abcfca7b23d8502b
franckrupin <frupin@protonmail.com>
2023-12-28 08:23:56 -0500

    Add unittests

---
+++ wazo_load_cli/wlctl/tests/test_compose.py
@@ -0,0 +1,39 @@
+import unittest
+import os
+
+from wlctl.modules.compose import DockerComposeGenerator  
+
+class TestDockerComposeGenerator(unittest.TestCase):
+
+    def setUp(self):
+        self.generator = DockerComposeGenerator(compose_file="test-docker-compose.yml")  
+
+    def tearDown(self):
+        if os.path.exists("test-docker-compose.yml"):
+            os.remove("test-docker-compose.yml")
+
+    def test_generate_service_config(self):
+        config = self.generator._generate_service_config(0)
+        self.assertIn("wlapi0", config)
+
+    def test_update_services(self):
+        self.generator._update_services(2)
+        self.assertEqual(len(self.generator.services["services"]), 2)
+        self.assertIn("wlapi0", self.generator.services["services"])
+        self.assertIn("wlapi1", self.generator.services["services"])
+
+    def test_generate_compose_file(self):
+        self.generator.generate_compose_file(2)
+        self.assertTrue(os.path.exists("test-docker-compose.yml"))
+        with open("test-docker-compose.yml", 'r') as f:
+            content = f.read()
+            self.assertIn("wlapi0:", content)
+            self.assertIn("wlapi1:", content)
+            self.assertIn("API_PORT", content)
+            self.assertIn("SIP_PORTS", content)
+            self.assertIn("MEDIA_PORTS", content)
+            self.assertIn("/etc/resolv.conf", content)
+            self.assertIn("/tmp/pulseaudio.socket", content)
+            self.assertIn("/opt/pulseaudio.client.conf", content)
+            self.assertIn("/debug.log", content)
+
