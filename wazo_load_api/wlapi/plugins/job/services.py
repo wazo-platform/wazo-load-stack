208a06eeb62c8ec609bd05b12a811fc6af07361d
franckrupin <frupin@protonmail.com>
2024-01-31 13:15:08 -0500

    Fix linter issues

--- wazo_load_api/wlapi/plugins/job/services.py
+++ wazo_load_api/wlapi/plugins/job/services.py
@@ -1,9 +1,9 @@
 # Copyright 2023 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import os
 import asyncio
 import subprocess
+
 
 async def run(cmd, environ):
     print(f"RECEIVED ENVIRON TYPE OF === {type(environ)}")
@@ -13,13 +13,9 @@ async def run(cmd, environ):
 
     try:
         process = await asyncio.create_subprocess_shell(
-            cmd,
-            env=environ,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
+            cmd, env=environ, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
-        
+
         stdout, stderr = await process.communicate()
         stdout_return = stdout.decode('utf-8').strip()
         stderr_return = stderr.decode('utf-8').strip()
@@ -27,14 +23,10 @@ async def run(cmd, environ):
         print(f"          STDOUT ::::::::::::: {stdout_return}")
         print(f"          STDERR ::::::::::::: {stderr_return}")
         print(f"     RETURN_CODE ::::::::::::: {stderr_return}")
-        
+
     except Exception as e:
         print(f"there was an error running {cmd} ::: {str(e)}")
         raise ValueError(f"Error running {cmd}: {str(e)}")
 
-    print(f"SUCCESSFULLY RAN ::::::::::::: {cmd} ::: {str(e)}")
-    return {
-            "stdout": stdout_return,
-            "stderr": stderr_return,
-            "returncode": return_code
-        }
+    print(f"SUCCESSFULLY RAN ::::::::::::: {cmd}")
+    return {"stdout": stdout_return, "stderr": stderr_return, "returncode": return_code}
