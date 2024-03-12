2aff3568587451f40d684c47da2a16e0c9e40630
franckrupin <frupin@protonmail.com>
2024-03-12 11:36:20 -0400

    Fix pilot for simple calls

--- wazo_load_pilot/plugins/pilot/pilot.py
+++ wazo_load_pilot/plugins/pilot/pilot.py
@@ -41,7 +41,7 @@ class WorkloadProcessor:
         try:
             cmd = await self.get_command(node)
             print(f"URLS FROM process_node ======= {cmd.urls}")
-            # print(cmd.send())
+
             responses = await cmd.send()
             print(f"SEND RETURNED ----------------- {responses}")
 
@@ -66,6 +66,7 @@ class WorkloadProcessor:
 
             if self.cancel_event.is_set():
                 break
+            await asyncio.sleep(0.2)
 
         try:
             await asyncio.gather(*coroutines)
@@ -78,7 +79,7 @@ class WorkloadProcessor:
         coroutines = []
         for x in range(self.scheduler_batch):
             if self.start_time + self.scheduler_duration < int(time.time()):
-                print("This load has reached its deadline, it about to terminate")
+                print("This load has reached its deadline, it is about to terminate")
                 break
             print(f"----- ITERATION #{x}")
             queue = await parse_config(data)
@@ -108,107 +109,6 @@ class WorkloadProcessor:
         except Exception as e:
             traceback.print_exc()
             print(f"An error occurred: {str(e)}")
-
-
-# semaphore = asyncio.Semaphore(1000)
-
-
-# def get_command(node):
-#     if 'cmd' not in node:
-#         raise KeyError('Missing required key "cmd"')
-#     command = node['cmd']
-#     print(f"COMMAND TO SEND --------------- {command}")
-
-#     env = node.get('env')
-#     shell_factory: ShellCmdFactory = ShellCmdFactory(
-#         cmd=command, env=env, cluster=cluster
-#     )
-#     return shell_factory.new()
-
-
-# async def process_node(node, ttl):
-#     """
-#     Coroutine that connects, processes and disconnect from the remote host. Connection duration
-#     is based on the TTL
-#     """
-#     print(f"LOAD READY TO PROCESS +++++++++ {node}")
-
-#     try:
-#         cmd = get_command(node)
-#         print(f"URLS FROM process_node ======= {cmd.urls}")
-#         # print(cmd.send())
-#         responses = await cmd.send()
-#         print(f"SEND RETURNED ----------------- {responses}")
-
-#         # Await on the TTL
-#         await asyncio.sleep(ttl)
-
-#     except Exception as e:
-#         traceback.print_exc()
-#         print(f"An error occurred in process_node: {str(e)}")
-
-
-# async def process_load(load):
-#     """
-#     Coroutine that processes a workload by creating a process_node coroutine for each node.
-#     This coroutine setup an additional queue that could be used for node coroutines
-#     to synchrinize.
-#     """
-
-#     coroutines = []
-
-#     nodes = load.get('load')
-#     # Loop for creating a coroutine for each node in the workload.
-#     for node in nodes:
-#         print(f"EXTRACTED NODES FROM process_load ====== {node}")
-#         ttl = 0
-#         print(f"TTL ============= {ttl}")
-#         async with semaphore:
-#             coroutine = asyncio.create_task(process_node(node, ttl))
-#             coroutines.append(coroutine)
-
-#     # The coroutine is waiting for all coroutines terminate
-#     try:
-#         await asyncio.gather(*coroutines)
-#     except Exception as e:
-#         traceback.print_exc()
-#         print(f"An error occurred: {str(e)}")
-
-
-# async def orchestrator(data, delay=0.0, iterations=2):
-#     """
-#     This coroutine consumes a queue containing the workload.
-#     When the orchestrator dequeue a load it sends it to the coroutine
-#     in charge of processing the load.
-#     """
-#     print("******STARTING SCHEDULE*********")
-#     coroutines = []
-#     for x in range(iterations):
-#         print(f"----- ITERATION #{x}")
-#         queue = await parse_config(data)
-#         print(f"PROCESSING QUEUE {queue.__str__}")
-#         while not queue.empty():
-#             async with semaphore:
-#                 load = queue.get_nowait()
-#                 queue.task_done()
-#                 coroutine = asyncio.create_task(process_load(load))
-#                 coroutines.append(coroutine)
-
-#             await asyncio.sleep(delay)
-#                 #await process_load(load)
-#         #while True:
-#         #    try:
-#         #        load = queue.get_nowait()
-#         #    except asyncio.QueueEmpty:
-#         #        break
-#         #    else:
-#         #        await process_load(load)
-#         #        queue.task_done()
-#     try:
-#         await asyncio.gather(*coroutines)
-#     except Exception as e:
-#         traceback.print_exc()
-#         print(f"An error occurred: {str(e)}")
 
 
 async def parse_config(yml):
